05bf4541b0ca0a5c4f7ddcec127b337489e04572
Jim <jimba.ed@outlook.com>
2022-10-13 16:53:47 -0400

    commit tree 9151316a4ae95e92457f18025e31c9329a9e3156

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    is_it_a_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def sides_greater_than_zero?
+    [a,b,c].all?(&:positive?)
+  end
+  def side_inequality?
+    a + b > c && b + c > a && a + c > b
+  end
+
+  def is_it_a_triangle
+    raise TriangleError unless sides_greater_than_zero? && side_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
